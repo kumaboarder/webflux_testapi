e1a1492f34b710e609fa9f2a7573e549a2ed47d3
Kuramoto <kumaboarder@gmail.com>
2019-07-29 23:58:51 +0900

    一通り動くものを作成

--- src/main/java/org/kura/webflux/UserController.java
+++ src/main/java/org/kura/webflux/UserController.java
@@ -4,25 +4,30 @@ import java.util.Random;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class UserController {
 	
-	@RequestMapping("/user/{id}")
-	public User getUser(@RequestParam("id") String id){
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable("id") String id){
 		User user = new User();
-		
+		user.setId(id);
+		user.setName("TEST " + id);
+		user.setEmail(id + "@test.com");
+		log.info("getUser :" + user.toString());
 		return user;
 	}
 	
 	@RequestMapping("/gettoken")
 	public String getToken(){
 		Random random = new Random();
-		String token = String.valueOf(random.nextInt(10));
+		String token = String.valueOf(random.nextInt(10000000) + 10000000);
+		log.info("getToken :" + token);
 
 		return token;
 	}
